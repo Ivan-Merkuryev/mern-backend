46d493f350dc6fe3001b4ddb6a91e0fb67da3291
Ivan-Merkuryev <129294036+Ivan-Merkuryev@users.noreply.github.com>
2023-11-04 16:35:45 +0300

    Delete controllers directory

--- controllers/index.js
+++
@@ -1,4 +0,0 @@
-export * as UserController from "./UserController.js";
-export * as PostController from "./PostController.js";
-export * as ProductController from './ProductController.js'
-export * as MemberController from './MemberController.js'
