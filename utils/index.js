ce4b6a80bbe7412bf4f6325eecf2a671af86dc1f
Ivan-Merkuryev <129294036+Ivan-Merkuryev@users.noreply.github.com>
2023-11-04 16:36:22 +0300

    Delete utils directory

--- utils/index.js
+++
@@ -1,5 +0,0 @@
-export { default as handleErrors } from "./handleErrors.js";
-export { default as checkAuth } from "./checkAuth.js";
-export { default as checkMember } from "./checkMember.js";
-export { default as checkFollowers } from './checkFollowers.js'
-
